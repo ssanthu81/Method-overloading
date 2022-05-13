2746c620fe1d0a299d4331c51badc44f74f03c6f
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:07:30 -0500

    Add files via upload

---
+++ Methodoverloading/Methodoverloading/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    public void t1()
+    {
+        Console.WriteLine("1st");
+    }
+    public void t1(int i)
+    {
+        Console.WriteLine("2nd ");
+    }
+    public void t1(string s)
+    {
+        Console.WriteLine("3rd");
+    }
+    public void t1(int i, string s)
+    {
+        Console.WriteLine("4th");
+    }
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.t1();
+        p.t1(10);
+        p.t1("xx");
+        p.t1(10, "ss");
+        
+        Console.ReadLine();
+    }
+}
+
